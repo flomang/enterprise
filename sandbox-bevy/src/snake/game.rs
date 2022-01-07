7219c1cc1f76716cc150dae9944ad9ba2e32d370
flomang <inbox.733@gmail.com>
2022-01-06 20:11:58 -0700

    Limpio.

--- sandbox-bevy/src/snake/game.rs
+++ sandbox-bevy/src/snake/game.rs
@@ -42,28 +42,7 @@ fn sprite_factory(material: &Handle<ColorMaterial>) -> SpriteBundle {
     }
 }
 
-// return random optional position
-// fn random_position(
-//     entities: Query<Entity, With<super::Position>>,
-//     mut positions: Query<&mut super::Position>,
-// ) -> Option<super::Position> {
-//     let entity_positions = entities
-//         .iter()
-//         .map(|e| *positions.get_mut(e).unwrap())
-//         .collect::<Vec<super::Position>>();
-
-//     let position = super::Position {
-//         x: (random::<f32>() * super::ARENA_WIDTH as f32) as i32,
-//         y: (random::<f32>() * super::ARENA_HEIGHT as f32) as i32,
-//     };
-
-//     match entity_positions.contains(&position) {
-//         true => None,
-//         false => Some(position),
-//     }
-// }
-
-fn random_position_off_screen(
+fn random_position(
     entities: Query<Entity, With<super::Position>>,
     mut positions: Query<&mut super::Position>,
 ) -> Option<super::Position> {
@@ -74,6 +53,7 @@ fn random_position_off_screen(
 
     let position = super::Position {
         x: super::ARENA_WIDTH as i32,
+        //x: (random::<f32>() * super::ARENA_WIDTH as f32) as i32,
         y: (random::<f32>() * super::ARENA_HEIGHT as f32) as i32,
     };
 
@@ -140,43 +120,13 @@ pub fn spawn_food(
     positions: Query<&mut super::Position>,
 ) {
     // can't spawn on existing entity
-    if let Some(position) = random_position_off_screen(entities, positions) {
+    if let Some(position) = random_position(entities, positions) {
         commands
             .spawn_bundle(sprite_factory(&materials.pizza))
             .insert(Food)
             .insert(position);
     }
 }
-
-// pub fn spawn_poison(
-//     mut commands: Commands,
-//     materials: Res<super::Materials>,
-//     entities: Query<Entity, With<super::Position>>,
-//     positions: Query<&mut super::Position>,
-// ) {
-//     // can't spawn on existing entity
-//     if let Some(position) = random_position(entities, positions) {
-//         commands
-//             .spawn_bundle(sprite_factory(&materials.pill))
-//             .insert(super::Poison)
-//             .insert(position);
-//     }
-// }
-
-// pub fn spawn_wormhole(
-//     mut commands: Commands,
-//     materials: Res<super::Materials>,
-//     entities: Query<Entity, With<super::Position>>,
-//     positions: Query<&mut super::Position>,
-// ) {
-//     // can't spawn on existing entity
-//     if let Some(position) = random_position(entities, positions) {
-//         commands
-//             .spawn_bundle(sprite_factory(&materials.cherry))
-//             .insert(super::Wormhole)
-//             .insert(position);
-//     }
-// }
 
 pub fn food_movement(
     mut commands: Commands,
@@ -197,7 +147,6 @@ pub fn snake_movement(
     mut heads: Query<(Entity, &mut super::SnakeHead)>,
     mut last_tail_position: ResMut<LastTailPosition>,
     mut positions: Query<&mut super::Position>,
-    mut game_over_writer: EventWriter<events::GameOverEvent>,
 ) {
     if let Some((head_entity, mut head)) = heads.iter_mut().next() {
         let segment_positions = segments
@@ -227,7 +176,6 @@ pub fn snake_movement(
             || (head_pos.x as u32) + 1 >= super::ARENA_WIDTH
             || (head_pos.y as u32) + 1 >= super::ARENA_HEIGHT
         {
-            //game_over_writer.send(events::GameOverEvent);
             head.direction = head.direction.opposite();
         }
 
@@ -241,81 +189,6 @@ pub fn snake_movement(
         last_tail_position.0 = Some(*segment_positions.last().unwrap());
     }
 }
-
-// pub fn snake_movement_input(
-//     segments: ResMut<SnakeSegments>,
-//     keyboard_input: Res<Input<KeyCode>>,
-//     mut heads: Query<(Entity, &mut super::SnakeHead)>,
-//     mut last_tail_position: ResMut<LastTailPosition>,
-//     mut positions: Query<&mut super::Position>,
-//     mut game_over_writer: EventWriter<events::GameOverEvent>,
-// ) {
-//     if let Some((head_entity, mut head)) = heads.iter_mut().next() {
-//         let mut keypressed = true;
-//         let dir: super::Direction = if keyboard_input.pressed(KeyCode::Left) {
-//             super::Direction::Left
-//         } else if keyboard_input.pressed(KeyCode::Down) {
-//             super::Direction::Down
-//         } else if keyboard_input.pressed(KeyCode::Up) {
-//             super::Direction::Up
-//         } else if keyboard_input.pressed(KeyCode::Right) {
-//             super::Direction::Right
-//         } else {
-//             keypressed = false;
-//             head.direction
-//         };
-
-//         if keypressed {
-//             if dir != head.direction.opposite() && dir != head.input_direction.opposite() {
-//                 head.input_direction = dir;
-//                 head.direction = dir;
-//             }
-
-//             let segment_positions = segments
-//                 .0
-//                 .iter()
-//                 .map(|e| *positions.get_mut(*e).unwrap())
-//                 .collect::<Vec<super::Position>>();
-//             let mut head_pos = positions.get_mut(head_entity).unwrap();
-
-//             match &head.direction {
-//                 super::Direction::Left => {
-//                     head_pos.x -= 1;
-//                 }
-//                 super::Direction::Right => {
-//                     head_pos.x += 1;
-//                 }
-//                 super::Direction::Up => {
-//                     head_pos.y += 1;
-//                 }
-//                 super::Direction::Down => {
-//                     head_pos.y -= 1;
-//                 }
-//             };
-
-//             if head_pos.x < 0
-//                 || head_pos.y < 0
-//                 || head_pos.x as u32 >= super::ARENA_WIDTH
-//                 || head_pos.y as u32 >= super::ARENA_HEIGHT
-//             {
-//                 game_over_writer.send(events::GameOverEvent);
-//             }
-
-//             if segment_positions.contains(&head_pos) {
-//                 game_over_writer.send(events::GameOverEvent);
-//             }
-
-//             segment_positions
-//                 .iter()
-//                 .zip(segments.0.iter().skip(1))
-//                 .for_each(|(pos, segment)| {
-//                     *positions.get_mut(*segment).unwrap() = *pos;
-//                 });
-
-//             last_tail_position.0 = Some(*segment_positions.last().unwrap());
-//         }
-//     }
-// }
 
 pub fn snake_movement_input(
     keyboard_input: Res<Input<KeyCode>>,
@@ -357,22 +230,6 @@ pub fn snake_eating(
     }
 }
 
-// pub fn snake_transporting(
-//     mut commands: Commands,
-//     mut warp_writer: EventWriter<events::WarpEvent>,
-//     warp_positions: Query<(Entity, &super::Position), With<super::Wormhole>>,
-//     head_positions: Query<&super::Position, With<super::SnakeHead>>,
-// ) {
-//     for head_pos in head_positions.iter() {
-//         for (ent, pos) in warp_positions.iter() {
-//             if pos == head_pos {
-//                 commands.entity(ent).despawn();
-//                 warp_writer.send(events::WarpEvent);
-//             }
-//         }
-//     }
-// }
-
 pub fn snake_dying(
     mut commands: Commands,
     mut game_over_writer: EventWriter<events::GameOverEvent>,
@@ -404,19 +261,6 @@ pub fn snake_growth(
         ));
     }
 }
-
-// pub fn snake_warp(
-//     mut commands: Commands,
-//     mut segments: ResMut<SnakeSegments>,
-//     mut warp_reader: EventReader<events::WarpEvent>,
-// ) {
-//     // on warp event and if there are more than 1 segments for the snake
-//     if warp_reader.iter().next().is_some() && segments.0.len() > 1 {
-//         if let Some(seg) = segments.0.pop() {
-//             commands.entity(seg).despawn();
-//         }
-//     }
-// }
 
 pub fn game_over(
     mut commands: Commands,
