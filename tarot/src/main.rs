d48397ada195757ad8210e7803dabf8f0855d06f
flomang <inbox.733@gmail.com>
2022-01-08 14:49:40 -0700

    Read card data from json file.

--- tarot/src/main.rs
+++ tarot/src/main.rs
@@ -1,12 +1,31 @@
 use bevy::prelude::*;
+use std::path::Path;
+use std::fs::File;
+use serde::Deserialize;
 
 mod game;
 
-fn main() {
-    let tarot_cards = game::MajorArcana::new();
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Card {
+    order: u8, 
+    title: String,
+    up: String,
+    reverse: String
+}
 
-    for o in tarot_cards.sorted_keys() {
-        println!("({}) {}", o, tarot_cards.card_title(*o));
-    }
-    println!("({}) {}", 0, tarot_cards.card_title(0))
+fn main() {
+    //let tarot_cards = game::MajorArcana::new();
+
+    //for o in tarot_cards.sorted_keys() {
+    //    println!("({}) {}", o, tarot_cards.card_title(*o));
+    //}
+    //println!("({}) {}", 0, tarot_cards.card_title(0));
+
+    let json_file_path = Path::new("assets/cards/modern-magick.json");
+    let file = File::open(json_file_path).expect("file not found");
+    let cards: Vec<Card> = serde_json::from_reader(file).expect("error while reading");
+   for card in cards {
+       println!("({}) {}", card.order, card.title)
+   }
 }
