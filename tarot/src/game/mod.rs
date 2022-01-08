e9a719e0091ecead6f8765af2d826fe813ade548
flomang <inbox.733@gmail.com>
2022-01-08 14:04:58 -0700

    Use immutable slices for the card titles.

--- tarot/src/game/mod.rs
+++ tarot/src/game/mod.rs
@@ -1,14 +1,14 @@
 use std::collections::HashMap;
 
-pub struct Card {
+pub struct Card<'a> {
     order: u8,
-    title: String,
-    up: String,
-    reverse: String,
+    title: &'a str,
+    up: &'a str,
+    reverse: &'a str,
 }
 
-impl Card {
-    pub fn new(order: u8, title: String, up: String, reverse: String) -> Card {
+impl<'a> Card<'a> {
+    pub fn new(order: u8, title: &'a str, up: &'a str, reverse: &'a str) -> Card<'a> {
         Card{
             order: order,
             title: title,
@@ -18,62 +18,53 @@ impl Card {
     }
 }
 
-pub struct MajorArcana {
-    hash_map: HashMap<u8, String>,
-    cards: HashMap<u8, Card>,
+pub struct MajorArcana<'a> {
+    card_titles: HashMap<u8, &'a str>,
 }
 
-impl MajorArcana {
-    pub fn new() -> MajorArcana {
+impl<'a> MajorArcana<'a> {
+    pub fn new() -> MajorArcana<'a> {
         MajorArcana{
-            hash_map: build_deck(),
-            cards: HashMap::new(),
+            card_titles: build_deck(),
         }
     }
 
     // TODO sort by order
     pub fn sorted_keys(&self) -> Vec<&u8> {
-        let mut orders: Vec<&u8> = self.hash_map.keys().collect();
+        let mut orders: Vec<&u8> = self.card_titles.keys().collect();
         orders.sort();
         orders
     }
 
-    pub fn card_title(&self, order: u8) -> &String {
-        let title = &self.hash_map[&order];
-        title
-    }
-
-    pub fn add_definition(&mut self, order: u8, up: String, reverse: String) {
-        let title = self.card_title(order);
-        let card = Card::new(order, title.clone(), up, reverse);
-        self.cards.insert(order, card);
+    pub fn card_title(&self, order: u8) -> &str {
+        self.card_titles[&order]
     }
 }
 
 
-fn build_deck() -> HashMap<u8, String> {
-    let mut cards: HashMap<u8, String> = HashMap::new();
-    cards.insert(0, String::from("The Fool"));
-    cards.insert(1, String::from("The Magician"));
-    cards.insert(2, String::from("The High Priestess"));
-    cards.insert(3, String::from("The Empress"));
-    cards.insert(4, String::from("The Emperor"));
-    cards.insert(5, String::from("The Hierophant"));
-    cards.insert(6, String::from("The Lovers"));
-    cards.insert(7, String::from("The Chariot"));
-    cards.insert(8, String::from("Strength"));
-    cards.insert(9, String::from("The Hermit"));
-    cards.insert(10, String::from("Wheel of Fortune"));
-    cards.insert(11, String::from("Justice"));
-    cards.insert(12, String::from("The Hanged Man"));
-    cards.insert(13, String::from("Death"));
-    cards.insert(14, String::from("Temperance"));
-    cards.insert(15, String::from("Devil"));
-    cards.insert(16, String::from("The Tower"));
-    cards.insert(17, String::from("The Star"));
-    cards.insert(18, String::from("The Moon"));
-    cards.insert(19, String::from("The Sun"));
-    cards.insert(20, String::from("Judgement"));
-    cards.insert(21, String::from("The Universe"));
+fn build_deck<'a>() -> HashMap<u8, &'a str> {
+    let mut cards: HashMap<u8, &str> = HashMap::new();
+    cards.insert(0, "The Fool");
+    cards.insert(1, "The Magician");
+    cards.insert(2, "The High Priestess");
+    cards.insert(3, "The Empress");
+    cards.insert(4, "The Emperor");
+    cards.insert(5, "The Hierophant");
+    cards.insert(6, "The Lovers");
+    cards.insert(7, "The Chariot");
+    cards.insert(8, "Strength");
+    cards.insert(9, "The Hermit");
+    cards.insert(10, "Wheel of Fortune");
+    cards.insert(11, "Justice");
+    cards.insert(12, "The Hanged Man");
+    cards.insert(13, "Death");
+    cards.insert(14, "Temperance");
+    cards.insert(15, "Devil");
+    cards.insert(16, "The Tower");
+    cards.insert(17, "The Star");
+    cards.insert(18, "The Moon");
+    cards.insert(19, "The Sun");
+    cards.insert(20, "Judgement");
+    cards.insert(21, "The Universe");
     cards
 }
