128a163c54aaf202a900df571944a25ba9939ef1
flomang <inbox.733@gmail.com>
2022-01-19 21:07:56 -0700

    Despawn all cards on game state exit.

--- tarot/src/game/tarot.rs
+++ tarot/src/game/tarot.rs
@@ -24,7 +24,8 @@ impl Plugin for GamePlugin {
             .add_system_set(
                 SystemSet::on_exit(GameState::Game)
                     .with_system(despawn_screen::<OnGameScreen>)
-                    .with_system(despawn_screen::<MainCamera>),
+                    .with_system(despawn_screen::<MainCamera>)
+                    .with_system(despawn_screen::<super::Card>),
             );
     }
 }
