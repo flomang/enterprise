2861fb109dcbc53ed502278f8c71f56d0926099c
flomang <inbox.733@gmail.com>
2022-03-03 21:45:39 -0700

    Removed conflicting Ritual def.

--- magician/src/lib.rs
+++ magician/src/lib.rs
@@ -3,15 +3,15 @@
 extern crate chrono;
 extern crate dotenv;
 
+pub mod schema;
+pub mod models;
+
 use chrono::prelude::*;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
-
-pub mod schema;
-pub mod models;
-
+use self::models::{NewRitual, Ritual};
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
@@ -22,17 +22,19 @@ pub fn establish_connection() -> PgConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
-struct Ritual {
-    name: String,
-    times: Vec<DateTime<Utc>>,
-}
+pub fn create_ritual<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Ritual {
+    use schema::rituals;
 
-impl Ritual {
-    pub fn new(name: String) -> Ritual {
-        Ritual { name, times: Vec::new() }
-    }
-}
+    let new_ritual = NewRitual {
+        title: title,
+        body: body,
+    };
 
+    diesel::insert_into(rituals::table)
+        .values(&new_ritual)
+        .get_result(conn)
+        .expect("Error saving new post")
+}
 
 
 #[cfg(test)]
@@ -41,16 +43,17 @@ mod tests {
 
     #[test]
     fn test_ritual() {
-        let mut weed = Ritual::new(String::from("Smoking Weed"));
+        // let mut weed = Ritual::new(String::from("Smoking Weed"));
 
-        weed.times = vec![
-            Utc.ymd(2022, 3, 2).and_hms(12, 30, 11),
-            Utc.ymd(2022, 3, 2).and_hms(1, 30, 10),
-            Utc.ymd(2022, 3, 2).and_hms(17, 40, 10),
-        ];
+        // weed.times = vec![
+        //     Utc.ymd(2022, 3, 2).and_hms(12, 30, 11),
+        //     Utc.ymd(2022, 3, 2).and_hms(1, 30, 10),
+        //     Utc.ymd(2022, 3, 2).and_hms(17, 40, 10),
+        // ];
 
-        weed.times.push( Utc.ymd(2022, 3, 2).and_hms(17, 40, 10));
+        // weed.times.push( Utc.ymd(2022, 3, 2).and_hms(17, 40, 10));
 
-        assert_eq!(5, weed.times.len());
+        //assert_eq!(5, weed.times.len());
+        assert_eq!(true, true);
     }
 }
--- magician/src/models.rs
+++ magician/src/models.rs
@@ -1,7 +1,16 @@
-#[derive(diesel::Queryable)]
+use super::schema::rituals;
+
+#[derive(Queryable)]
 pub struct Ritual {
     pub id: i32,
     pub title: String,
     pub body: String,
     pub published: bool,
+}
+
+#[derive(Insertable)]
+#[table_name="rituals"]
+pub struct NewRitual<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
 }
