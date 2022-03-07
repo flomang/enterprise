4357331fd202623f8bc579c4a132c00250b1eb99
flomang <inbox.733@gmail.com>
2022-03-06 21:04:08 -0700

    Delete ritual should casscade to ritual times.

--- magician/migrations/2022-03-05-010513_create_ritual_times/up.sql
+++ magician/migrations/2022-03-05-010513_create_ritual_times/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE ritual_times (
   id SERIAL PRIMARY KEY,
-  ritual_id INTEGER NOT NULL REFERENCES rituals(id),
+  ritual_id INTEGER NOT NULL REFERENCES rituals(id) ON DELETE CASCADE,
   created_on TIMESTAMP NOT NULL
 );
