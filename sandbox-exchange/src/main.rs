b12d5aca57d2ec93896e881ab649e4f804d2d703
flomang <inbox.733@gmail.com>
2022-03-16 21:26:40 -0600

    All routes should be scoped with "api".

--- sandbox-exchange/src/main.rs
+++ sandbox-exchange/src/main.rs
@@ -1,13 +1,14 @@
-use actix_web::{error, post, patch, delete, web, App, Error, HttpResponse, HttpServer, Result, Responder};
-use futures::StreamExt;
-use std::sync::Mutex;
+use actix_web::{
+    delete, patch, post, web, App, HttpServer, Responder, Result,
+};
 use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
 
-use std::time::SystemTime;
-use exchange::engine;
 use engine::domain::OrderSide;
 use engine::orderbook::Orderbook;
 use engine::orders;
+use exchange::engine;
+use std::time::SystemTime;
 
 // please keep these organized while editing
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
@@ -62,10 +63,11 @@ struct AppState {
     order_book: Mutex<Orderbook<BrokerAsset>>,
 }
 
-const MAX_SIZE: usize = 262_144; // max payload size is 256k
-
 #[post("/orders")]
-async fn post_order(state: web::Data<AppState>, req: web::Json<OrderRequest>) -> Result<impl Responder> {
+async fn post_order(
+    state: web::Data<AppState>,
+    req: web::Json<OrderRequest>,
+) -> Result<impl Responder> {
     let order_asset_opt = BrokerAsset::from_string(&req.order_asset);
     let price_asset_opt = BrokerAsset::from_string(&req.price_asset);
     let side_opt = OrderSide::from_string(&req.side);
@@ -74,45 +76,48 @@ async fn post_order(state: web::Data<AppState>, req: web::Json<OrderRequest>) ->
     match (order_asset_opt, price_asset_opt, side_opt) {
         (Some(order_asset), Some(price_asset), Some(side)) => {
             let order = if let Some(price) = price_opt {
-               orders::new_limit_order_request(
-                  order_asset,
-                  price_asset,
-                  side,
-                  price,
-                  req.qty,
-                  SystemTime::now())
+                orders::new_limit_order_request(
+                    order_asset,
+                    price_asset,
+                    side,
+                    price,
+                    req.qty,
+                    SystemTime::now(),
+                )
             } else {
                 orders::new_market_order_request(
-                   order_asset,
-                   price_asset,
-                   side,
-                   req.qty,
-                   SystemTime::now())
+                    order_asset,
+                    price_asset,
+                    side,
+                    req.qty,
+                    SystemTime::now(),
+                )
             };
 
             let mut book = state.order_book.lock().unwrap();
             let res = book.process_order(order);
             println!("Results => {:?}", res);
-            Ok(web::Json("ding")) 
+            Ok(web::Json("ding"))
         }
-        (None, _, _) => 
-
-            Ok(web::Json("bad order asset")),
-        (_, None, _) => 
-            Ok(web::Json("bad price asset")),
-        (_, _, None) => 
-            Ok(web::Json("bad side")),
+        (None, _, _) => Ok(web::Json("bad order asset")),
+        (_, None, _) => Ok(web::Json("bad price asset")),
+        (_, _, None) => Ok(web::Json("bad side")),
     }
 }
 
 #[patch("/orders/{id}")]
-async fn patch_order(path: web::Path<u64>, state: web::Data<AppState>, req: web::Json<AmendOrderRequest>) -> Result<impl Responder> {
+async fn patch_order(
+    path: web::Path<u64>,
+    state: web::Data<AppState>,
+    req: web::Json<AmendOrderRequest>,
+) -> Result<impl Responder> {
     let side_opt = OrderSide::from_string(&req.side);
     let id = path.into_inner();
 
     match side_opt {
-        Some(side) =>  {
-            let order = orders::amend_order_request(id, side, req.price, req.qty, SystemTime::now());
+        Some(side) => {
+            let order =
+                orders::amend_order_request(id, side, req.price, req.qty, SystemTime::now());
             let mut book = state.order_book.lock().unwrap();
             let res = book.process_order(order);
             Ok(web::Json(format!("{:?}", res))) //
@@ -122,16 +127,21 @@ async fn patch_order(path: web::Path<u64>, state: web::Data<AppState>, req: web:
 }
 
 #[delete("/orders/{id}")]
-async fn delete_order(path: web::Path<u64>, state: web::Data<AppState>, req: web::Json<CancelOrderRequest>) -> Result<impl Responder> {
+async fn delete_order(
+    path: web::Path<u64>,
+    state: web::Data<AppState>,
+    req: web::Json<CancelOrderRequest>,
+) -> Result<impl Responder> {
     let side_opt = OrderSide::from_string(&req.side);
     let id = path.into_inner();
 
     match side_opt {
-        Some(side) =>  {
+        Some(side) => {
             let order = orders::limit_order_cancel_request(id, side);
             let mut book = state.order_book.lock().unwrap();
             let res = book.process_order(order);
-            Ok(web::Json("what now".to_string())) 
+            println!("{:?}", res);
+            Ok(web::Json("what now".to_string()))
         }
         None => Ok(web::Json("side must be 'bid' or 'ask'".to_string())),
     }
@@ -140,14 +150,17 @@ async fn delete_order(path: web::Path<u64>, state: web::Data<AppState>, req: web
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let order_book = Orderbook::new(BrokerAsset::BTC, BrokerAsset::USD);
-    let data = web::Data::new(AppState {order_book: Mutex::new(order_book)});
+    let data = web::Data::new(AppState {
+        order_book: Mutex::new(order_book),
+    });
 
-    HttpServer::new( move || {
-        App::new()
-            .app_data(data.clone())
-            .service(post_order)
-            .service(patch_order)
-            .service(delete_order)
+    HttpServer::new(move || {
+        App::new().app_data(data.clone()).service(
+            web::scope("/api")
+                .service(post_order)
+                .service(patch_order)
+                .service(delete_order),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
