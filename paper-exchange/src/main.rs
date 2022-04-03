684e76e40f4037d3c7f79417b49b1c50cd2f25bc
flomang <inbox.733@gmail.com>
2022-04-02 19:34:25 -0600

    The paper exchange project should just be a binary.

--- paper-exchange/src/main.rs
+++ paper-exchange/src/main.rs
@@ -47,7 +47,7 @@ async fn main() -> std::io::Result<()> {
                     .service(paper::delete_order),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 3001))?
     .run()
     .await
 }
