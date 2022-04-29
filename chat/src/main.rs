9953801161278212e352ce87c0221e433672051b
flomang <inbox.733@gmail.com>
2022-04-28 21:06:07 -0600

    Revert of basic chat app.

--- chat/src/main.rs
+++ chat/src/main.rs
@@ -1,58 +1,75 @@
-use std::sync::{
-    atomic::{AtomicUsize},
-    Arc,
+use std::{
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    time::Instant,
 };
-use std::env;
 
-use actix::Actor;
-use actix_cors::Cors;
-use actix_web::{http, web, middleware::Logger, App, HttpServer};
-use dotenv::dotenv;
+use actix::*;
+use actix_files::{Files, NamedFile};
+use actix_web::{
+    middleware::Logger, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use actix_web_actors::ws;
 
-mod asteroid;
-mod routes;
+mod server;
+mod session;
 
-use asteroid::server;
+async fn index() -> impl Responder {
+    NamedFile::open_async("./static/index.html").await.unwrap()
+}
+
+/// Entry point for our websocket route
+async fn chat_route(
+    req: HttpRequest,
+    stream: web::Payload,
+    srv: web::Data<Addr<server::ChatServer>>,
+) -> Result<HttpResponse, Error> {
+    ws::start(
+        session::WsChatSession {
+            id: 0,
+            hb: Instant::now(),
+            room: "Main".to_owned(),
+            name: None,
+            addr: srv.get_ref().clone(),
+        },
+        &req,
+        stream,
+    )
+}
+
+/// Displays state
+async fn get_count(count: web::Data<AtomicUsize>) -> impl Responder {
+    let current_count = count.load(Ordering::SeqCst);
+    format!("Visitors: {}", current_count)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok();
-
-    // log info 
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    // App state
-    // We are keeping a count of the number of visitors
+    // set up applications state
+    // keep a count of the number of visitors
     let app_state = Arc::new(AtomicUsize::new(0));
 
-    // Start chat server actor
+    // start chat server actor
     let server = server::ChatServer::new(app_state.clone()).start();
 
-    // Create Http server with websocket support
-    HttpServer::new(move || {
-        let cors = Cors::default()
-            .allowed_origin(&env::var("CLIENT_HOST").unwrap())
-            .allow_any_method()
-            .allowed_headers(vec![
-                http::header::AUTHORIZATION,
-                http::header::ACCEPT,
-                http::header::CONTENT_TYPE,
-            ])
-            .max_age(3600);
+    log::info!("starting HTTP server at http://localhost:8080");
 
+    HttpServer::new(move || {
         App::new()
-            .wrap(cors)
+            .app_data(web::Data::from(app_state.clone()))
+            .app_data(web::Data::new(server.clone()))
+            .service(web::resource("/").to(index))
+            .route("/count", web::get().to(get_count))
+            .route("/ws", web::get().to(chat_route))
+            .service(Files::new("/static", "./static"))
             .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))
-            .data(app_state.clone())
-            .data(server.clone())
-            // websocket
-            .service(web::resource("/ws/").to(routes::chat_route))
-            // routes
-            .service(routes::bangs)
-            .service(routes::moonbang)
     })
-    .bind("0.0.0.0:8080")?
+    .workers(2)
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
