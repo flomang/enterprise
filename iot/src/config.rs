3d1f9561ce8587902599a3079610ff4a88e9bf0d
flomang <inbox.733@gmail.com>
2022-05-22 21:11:30 -0600

    Scope api prefix in url path.

--- iot/src/config.rs
+++ iot/src/config.rs
@@ -7,7 +7,7 @@ use actix_web::web;
 pub fn config_services(cfg: &mut web::ServiceConfig) {
     log::info!("Configuring routes...");
     cfg.service(
-        web::scope("")
+        web::scope("/api")
             .service(ping_controller::healthz)
             .service(web::scope("/upload").service(media_controller::upload))
             .service(web::scope("/download").service(media_controller::download)),
